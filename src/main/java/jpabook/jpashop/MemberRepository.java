6f8d70ea7a964e8340d577ff26f660c819725628
YHS <dlddu7064@naver.com>
2024-01-16 10:47:19 +0900

    default-setting

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext // 스프링부트가 Entity Manager를 자동으로 생성해줌.
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
